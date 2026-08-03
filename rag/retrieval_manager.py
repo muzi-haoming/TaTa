0047ed8a1864017dcaed64260ff5189cb0fe4865
muzi-haoming <56908149+muzi-haoming@users.noreply.github.com>
2026-08-03 19:30:31 +1000

    项目重构: 暂存

--- rag/retrieval_manager.py
+++ rag/retrieval_manager.py
@@ -1,17 +1,24 @@
 from model import PreRetrievalLLM
+from .bm25_retriever import BM25Retriever
+from .vector_retriever import VectorRetriever
 
 
 class RetrievalManager:
 
     def __init__(self):
         self.pre_retrieval_llm = PreRetrievalLLM()
+        self.bm25_retriever = BM25Retriever()
+        self.vector_retriever = VectorRetriever()
 
 
     def retrieve(self, query: str) -> list[str]:
         """
         整个检索流程:
-        1. 使用 PreRetrievalLLM 对用户查询进行改写和拆分，生成多个查询版本。
-        2. 对每个查询版本进行混合检索(BM25 + 向量)，获取相关文档。
-        3. 将所有检索结果进行去重和排序，返回最终的文档列表。
-        4. 使用重排模型对检索结果进行重排，提升结果的相关性和准确性。
+        1. 混合检索 (BM25 检索向量检索)
+        2. 去重和排序，返回最终的文档列表
+        3. 使用重排模型对检索结果进行重排，提升结果的相关性和准确性
         """
+        results = []
+        results.extend(self.bm25_retriever.retrieve(query=query))
+        results.extend(self.vector_retriever.retrieve(query=query))
+        
